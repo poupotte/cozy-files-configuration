b7d3ba39be2913bd996ab7e85cdae58d1a9f7cba
poupotte <bellot.zoe@gmail.com>
2013-12-09 16:38:29 +0100

    recover url, password and device name value

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.textinput import TextInput
+from kivy.properties import *
 
 
-class Configuration(GridLayout):
-    pass
+class Configuration(AnchorLayout):
+    url = ObjectProperty()
+    pwd = ObjectProperty()
+    name = ObjectProperty()
+
+    def install(self):
+        print self.url.text
+        print self.pwd.text
+        print self.name.text
+        pass
 
 
 class ConfigurationApp(App):
