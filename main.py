f60ac0df4a5335d8be7c1c77f13d1f4d6696f53e
poupotte <bellot.zoe@gmail.com>
2013-12-10 11:39:10 +0100

    init device and add replication from local

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.progressbar import ProgressBar
 from kivy.properties import *
 from requests import post
-from replication import replicate_to_local
+from replication import replicate_to_local, recover_progression, init_database, init_device, replicate_from_local
 from replication import recover_progression
 from couchdb import Database, Server
 from kivy.clock import Clock
@@ -29,136 +29,64 @@ class Configuration(AnchorLayout):
         if name is "" or pwd is "" or url is "":            
             self.error.text = 'Tous les champs doivent etre remplis'
             pass
-        try:  
-            data = {'login': name}
-            r = post(url + '/device/', data=data, auth=('owner', pwd))                 
-            if r.status_code == 401:
-                self.error.text = "L'url et le mot de passe de votre cozy \n           ne correspondent pas"
-                self.error.texture_update()
-                self.error.anchors
-                pass
-            elif r.status_code == 400:
-                self.error.text = 'Ce nom est deja utilise par un autre device'
-                self.error.texture_update()
-                self.error.anchors
+        else:
+            url = self._normalize_url(url)
+            if not url:        
+                self.error.text = "L'url de votre cozy n'est pas correcte"
                 pass
             else:
-                self.progress.value = 10
-                self.error.text = ""
-                self.error.texture_update()
-                self.error.anchors
-                self._init_database()                
-                self.progress.value = 15
-                data = json.loads(r.content)
-                replicate_to_local(url, name, data['password'], data['id']) 
-                Clock.schedule_interval(self.progress_bar, 1/25)
-        except Exception, e:
-            print e
-            self.error.text = "Verifiez l'url de votre cozy"
-            self.error.texture_update()
-            self.error.anchors
-            pass
+                try:  
+                    data = {'login': name}
+                    r = post(url + '/device/', data=data, auth=('owner', pwd)) 
+                except Exception, e:
+                    print e
+                    self.error.text = "Verifiez l'url de votre cozy"
+                    self.error.texture_update()
+                    self.error.anchors
+                    pass            
+                if r.status_code == 401:
+                    self.error.text = "L'url et le mot de passe de votre cozy \n           ne correspondent pas"
+                    self.error.texture_update()
+                    self.error.anchors
+                    pass
+                elif r.status_code == 400:
+                    self.error.text = 'Ce nom est deja utilise par un autre device'
+                    self.error.texture_update()
+                    self.error.anchors
+                    pass
+                else:
+                    self.progress.value = 10
+                    self.error.text = ""
+                    self.error.texture_update()
+                    self.error.anchors
+                    init_database()                
+                    self.progress.value = 15
+                    data = json.loads(r.content)
+                    replicate_to_local(url, name, data['password'], data['id'])  
+                    err = init_device(url, data['password'], data['id'])
+                    if err:
+                        self.error.text = err
+                        self.error.texture_update()
+                        self.error.anchors
+                        return False 
+                    else:
+                        replicate_from_local(url, name, data['password'], data['id'])
+                        Clock.schedule_interval(self.progress_bar, 1/25)
+
+        
 
     def progress_bar(self, dt):
         progress = recover_progression()
-        if progress > 0.494:
+        if progress > 0.98:
             return False
         self.progress.value = 15 + 75*progress
 
-    def _init_database(self):
-        server = Server('http://localhost:5984/')
-        # Read file
-        f = open('/etc/cozy/cozy-files/couchdb.login')
-        lines = f.readlines()
-        f.close()
-        username = lines[0].strip()
-        password = lines[1].strip()
-        # Create database
-        db = server.create(database)
-
-        db["_design/device"] = {
-            "views": {
-                "all": {
-                    "map": """function (doc) {
-                                  if (doc.docType === \"Device\") {
-                                      emit(doc.id, doc) 
-                                  }
-                              }"""
-                        },
-                "byUrl": {
-                    "map": """function (doc) {
-                                  if (doc.docType === \"Device\") {
-                                      emit(doc.url, doc) 
-                                  }
-                              }"""
-                        }
-                    }
-                }
-
-        db["_design/folder"] = {
-            "views": {
-                "all": {
-                    "map": """function (doc) {
-                                  if (doc.docType === \"Folder\") {
-                                      emit(doc.id, doc) 
-                                  }
-                               }"""
-                        },
-                "byFolder": {
-                    "map": """function (doc) {
-                                  if (doc.docType === \"Folder\") {
-                                      emit(doc.path, doc) 
-                                  }
-                              }"""
-                        },
-                "byFullPath": {
-                    "map": """function (doc) {
-                                  if (doc.docType === \"Folder\") {
-                                      emit(doc.path + '/' + doc.name, doc) 
-                                  }
-                              }"""
-                        }
-                    }
-                }
-
-        db["_design/file"] = {
-            "views": {
-                "all": {
-                    "map": """function (doc) {
-                                  if (doc.docType === \"File\") {
-                                      emit(doc.id, doc) 
-                                  }
-                               }"""
-                        },
-                "byFolder": {
-                    "map": """function (doc) {
-                                  if (doc.docType === \"File\") {
-                                      emit(doc.path, doc) 
-                                  }
-                              }"""
-                        },
-                "byFullPath": {
-                    "map": """function (doc) {
-                                  if (doc.docType === \"File\") {
-                                      emit(doc.path + '/' + doc.name, doc) 
-                                  }
-                              }"""
-                        }
-                    }
-                }
-
-        db["_design/binary"] = {
-            "views": {
-                "all": {
-                    "map": """function (doc) {
-                                  if (doc.docType === \"Binary\") {
-                                      emit(doc.id, doc) 
-                                  }
-                               }"""
-                        }
-                    }
-                }
-
+    def _normalize_url(self, url):
+        url_parts = url.split('/')
+        for part in url_parts:
+            if part.find('cozycloud.cc') is not -1:
+                return 'https://%s' %part
+        return False
 
 
 class ConfigurationApp(App):
--- replication.py
+++ replication.py
@@ -5,8 +5,9 @@ try:
     import simplejson as json
 except ImportError:
     import json # Python 2.6
-
 import requests
+import os
+
 database = "cozy-files"
 server = Server('http://localhost:5984/')
 
@@ -18,11 +19,12 @@ def replicate_to_local(url, device, pwdDevice, idDevice):
     source = "https://%s:%s@%s/cozy" % (device, pwdDevice, url[2])
     server.replicate(source, target, continuous=True, filter="%s/filter" %idDevice)
 
-def replicate_from_local(self):
+def replicate_from_local(url, device, pwdDevice, idDevice):
+    (username, password) = _get_credentials()
     source = 'http://%s:%s@localhost:5984/%s' % (username, password, database)
-    url = self.url.split('/')
-    target = "https://%s:%s@%s/cozy" % (self.device, self.pwdDevice, url[2])
-    self.rep = self.server.replicate(source, target, continuous=True, filter="%s/filter" %self.idDevice)
+    url = url.split('/')
+    target = "https://%s:%s@%s/cozy" % (device, pwdDevice, url[2])
+    server.replicate(source, target, continuous=True, filter="%s/filter" %idDevice)
 
 def recover_progression():
     url = 'http://localhost:5984/_active_tasks'
@@ -32,6 +34,133 @@ def recover_progression():
     for rep in replications:
         progress = progress + rep["progress"]
     return progress/200.
+
+def init_database():
+    # Create database
+    db = server.create(database)
+
+    db["_design/device"] = {
+        "views": {
+            "all": {
+                "map": """function (doc) {
+                              if (doc.docType === \"Device\") {
+                                  emit(doc.id, doc) 
+                              }
+                          }"""
+                    },
+            "byUrl": {
+                "map": """function (doc) {
+                              if (doc.docType === \"Device\") {
+                                  emit(doc.url, doc) 
+                              }
+                          }"""
+                    }
+                }
+            }
+
+    db["_design/folder"] = {
+        "views": {
+            "all": {
+                "map": """function (doc) {
+                              if (doc.docType === \"Folder\") {
+                                  emit(doc.id, doc) 
+                              }
+                           }"""
+                    },
+            "byFolder": {
+                "map": """function (doc) {
+                              if (doc.docType === \"Folder\") {
+                                  emit(doc.path, doc) 
+                              }
+                          }"""
+                    },
+            "byFullPath": {
+                "map": """function (doc) {
+                              if (doc.docType === \"Folder\") {
+                                  emit(doc.path + '/' + doc.name, doc) 
+                              }
+                          }"""
+                    }
+                }
+            }
+
+    db["_design/file"] = {
+        "views": {
+            "all": {
+                "map": """function (doc) {
+                              if (doc.docType === \"File\") {
+                                  emit(doc.id, doc) 
+                              }
+                           }"""
+                    },
+            "byFolder": {
+                "map": """function (doc) {
+                              if (doc.docType === \"File\") {
+                                  emit(doc.path, doc) 
+                              }
+                          }"""
+                    },
+            "byFullPath": {
+                "map": """function (doc) {
+                              if (doc.docType === \"File\") {
+                                  emit(doc.path + '/' + doc.name, doc) 
+                              }
+                          }"""
+                    }
+                }
+            }
+
+    db["_design/binary"] = {
+        "views": {
+            "all": {
+                "map": """function (doc) {
+                              if (doc.docType === \"Binary\") {
+                                  emit(doc.id, doc) 
+                              }
+                           }"""
+                    }
+                }
+            }
+
+def init_device(url, pwdDevice, idDevice):
+    db = server[database]
+    res = db.view("device/all")
+    if not res:
+        init_device(url, pwdDevice, idDevice)
+    else:
+        for device in res:
+            device = device.value
+            # Update device
+            folder = "%s/cozy-files" % os.environ['HOME']
+            device['password'] = pwdDevice
+            device['change'] = 0
+            device['url'] = url
+            device['folder'] = folder
+            db.save(device)
+            # Generate filter
+            filter = """function(doc, req) {
+                    if(doc._deleted) {
+                        return true; 
+                    }
+                    if ("""
+            for docType in device["configuration"]:
+                filter = filter + "(doc.docType && doc.docType === \"%s\") ||" %docType
+            filter = filter[0:-3]
+            filter = filter + """){
+                        return true; 
+                    } else { 
+                        return false; 
+                    }
+                }"""
+            doc = {
+                "_id": "_design/%s" % idDevice,
+                "views": {},
+                "filters": {
+                    "filter": filter
+                    }
+                }
+            db.save(doc) 
+            return False
 
 
 def _get_credentials():
